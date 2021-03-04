a8a92bc25668cf88f1b698b881e1224010c7de09
LonelyHao <16465666+zlhdd108@users.noreply.github.com>
2021-03-04 08:56:57 +0800

    Update entrypoint.sh
    crontab update

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,15 +6,15 @@ if [ ${enable_52pojie} ];then
 fi
 
 if [ ${enable_bilibili} ];then
-  echo "*/30 7-22 * * *       node /AutoSignMachine/index.js bilibili --cookies ${cookies} --username ${username} --password ${password} ${othercfg}" >> /var/spool/cron/crontabs/root
+  echo "*/30 6-23 * * *       node /AutoSignMachine/index.js bilibili --cookies ${cookies} --username ${username} --password ${password} ${othercfg}" >> /var/spool/cron/crontabs/root
 fi
 
 if [ ${enable_iqiyi} ];then
-  echo "*/30 7-22 * * *       node /AutoSignMachine/index.js iqiyi --P00001 ${P00001} --P00PRU ${P00PRU} --QC005 ${QC005}  --dfp ${dfp}" >> /var/spool/cron/crontabs/root
+  echo "*/30 6-23 * * *       node /AutoSignMachine/index.js iqiyi --P00001 ${P00001} --P00PRU ${P00PRU} --QC005 ${QC005}  --dfp ${dfp}" >> /var/spool/cron/crontabs/root
 fi
 
 if [ ${enable_unicom} ];then
-  echo "*/30 7-22 * * *       node /AutoSignMachine/index.js unicom --cookies ${cookies} --user ${user} --password ${password} --appid ${appid}" >> /var/spool/cron/crontabs/root
+  echo "*/30 6-23 * * *       node /AutoSignMachine/index.js unicom --cookies ${cookies} --user ${user} --password ${password} --appid ${appid}" >> /var/spool/cron/crontabs/root
 fi
 
 /usr/sbin/crond -S -c /var/spool/cron/crontabs -f -L /dev/stdout
